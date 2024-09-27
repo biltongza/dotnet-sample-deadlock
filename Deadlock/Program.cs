789e24a070373161e1a4439dbea5d6fc833e0708
Logan Dam <dam.loganza@gmail.com>
2024-09-27 08:33:16 +0200

    Add project files.

---
+++ Deadlock/Program.cs
@@ -0,0 +1,28 @@
+﻿var _lock1 = new object();
+var _lock2 = new object();
+
+var t1 = Task.Run(() =>
+{
+    lock (_lock1)
+    {
+        //Thread.Sleep(1);
+        lock (_lock2)
+        {
+            Console.WriteLine("Hello from task 1!");
+        }
+    }
+});
+
+var t2 = Task.Run(() =>
+{
+    lock (_lock2)
+    {
+        //Thread.Sleep(1);
+        lock (_lock1)
+        {
+            Console.WriteLine("Hello from task 2!");
+        }
+    }
+});
+
+await Task.WhenAll(t1, t2);
